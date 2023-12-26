3b38fa7dc9e649b11f37a95e6b79e944fdc105c9
Илья <149475177+Kusok0vody@users.noreply.github.com>
2023-12-26 21:25:50 +0700

    обновление + какаито функция Артёма
    ыыыыы

--- code_folder/sensitivity.py
+++ code_folder/sensitivity.py
@@ -3,8 +3,11 @@ import copy
 from tqdm.notebook import tqdm
 if __name__  == '__main__':
     import direct_problem as direct
+    import inverse_problem as inv
 else:
     from code_folder import direct_problem as direct
+    from code_folder import inverse_problem as inv
+
 
 def sense_one_point(r:float, param:list, change_num:int, delta:float, method:str):
     '''Возвращает чувствительность кажущегося сопротивления, посчитанного по method при изменении параметра change_num на delta относительно param
@@ -20,36 +23,38 @@ def sense_one_point(r:float, param:list, change_num:int, delta:float, method:str
     resistance1=direct.calculate_apparent_resistance(changed_param,method,r,10*int(1+r/200))
     return param[change_num]/resistance0*(resistance1-resistance0)/(delta)
 
+
 def sense_line(r_array:np.array, param:list, change_num:int, delta:float, method:str):
-    
+  
 
     n=[]
     for r_i in r_array:
         n.append(sense_one_point(r_i,param,change_num,delta,method))
     return n
 
-def matrix_models(parameters:list, layer_num:int, size:int, method:str, dh:list=None, drho:list=None) -> np.ndarray:
+
+def matrix_models(r:np.ndarray, parameters:list, layers_change:tuple, size:int, method:str, dh:list=None, drho:list=None) -> np.ndarray:
   """
   Возвращает матрицу ранга size, содержающие высчитанные в прямой задаче модели
   параметры, изменяемые с шагом dh и drho
 
   Параметры:
   ----------
+  r: np.array
+    Полуразнос
   parameters:list
-  Список входных параметров
-  layer_num:int
-  Номер слоя, который подвергается (XD подвергается :D) изменению
+    Список входных параметров
+  layers_change:tuple
+    Кортеж номеров параметров, которые подвергаются (XD подвергаются :D) изменению на dh или drho
   size:int
-  Размер получаемой матрицы
+    Размер получаемой матрицы
   method:str
-  Метод вычисления: по потенциалу(U) или по полю(E)
+    Метод вычисления: по потенциалу(U) или по полю(E)
   dh:list
-  Список высчитанных шагов по оси Ох (необязателен)
+    Список высчитанных шагов по оси Ох (необязателен)
   drho:list
-  Список высчитанных шагов по оси Оy (необязателен)
+    Список высчитанных шагов по оси Оy (необязателен)
   """
-
-  r=np.logspace(-1,2,100)
 
   if drho==None:
     drho = []
@@ -69,7 +74,15 @@ def matrix_models(parameters:list, layer_num:int, size:int, method:str, dh:list=
   for i in range(size):
     g = []
     for j in range(size):
-      g.append(parameters[:2*layer_num-2]+[drho[i]*parameters[2*layer_num-2]]+[dh[j]*parameters[2*layer_num-1]]+parameters[2*layer_num:])
+      parameters_g = []
+      for k in range(len(parameters)):
+        if k==layers_change[1] and k%2!=0:
+          parameters_g.append(parameters[k]+dh[j])
+        if k==layers_change[0] and k%2==0:
+          parameters_g.append(parameters[k]+drho[i])
+        else:
+          parameters_g.append(parameters[k])
+      g.append(parameters_g)
     func_param.append(g)
   func_param = np.array(func_param)
 
@@ -87,6 +100,55 @@ def matrix_models(parameters:list, layer_num:int, size:int, method:str, dh:list=
       a.append(resist[i][j*100:100*(j+1)])
     res.append(np.array(a))
   return np.array(res)
+
+
+def error_map_by_Artyom(r:np.ndarray,
+                        param_:list, 
+                        change_indexes_:list, 
+                        size:int, 
+                        method:str, 
+                        change: float=10):
+    '''  Строит карту ошибок относительно выбранной модели среды при изменении двух выбранных параметров  
+    
+    Parameters
+    ----------
+    r: np.array
+        Полуразнос
+    param_: list
+        Список параметров среды формой (2N-1), ultN -количество слоёв в модели. param[2*(i-1)]=rhoa_i, i=1, ..., N; param[2*(i-1)+1] = thickness_i, i=1, ..., N-1
+    change_indexes_: int
+        Отвечает за выбор изменяемых параметров
+    size: int
+        Определяет длину стороны картинки в пикселях
+    method: str
+        Переменная отвечающая за выбор по полю ("E") или потенциалу ("U") будет считаться кажущееся сопротивление
+    change: float
+        Определяет изменение параметро - от /change до *change
+    '''
+
+    etalon_resistance = np.zeros(shape=r.shape[0])
+    for k in range(r.shape[0]):
+        etalon_resistance[k] = direct.calculate_apparent_resistance(param_, method, r[k], 10+int(r[k]/200))
+
+    local_param = copy.copy(param_)
+    new_first_param = np.logspace(np.log10(param_[change_indexes_[0]]/change), np.log10(param_[change_indexes_[0]]*change), size)
+    new_second_param = np.logspace(np.log10(param_[change_indexes_[1]]/change), np.log10(param_[change_indexes_[1]]*change), size)
+    
+    loss_map = np.zeros(shape=(size, size))
+
+    for i in range(size):
+        for j in range(size):
+            local_param[change_indexes_[0]] = new_first_param[i] 
+            local_param[change_indexes_[1]] = new_second_param[j] 
+            
+            current_resistance = np.zeros(shape=r.shape[0])
+            for k in range(r.shape[0]):
+                current_resistance[k]=direct.calculate_apparent_resistance(local_param, method, r[k], 10+int(r[k]/200))
+
+            loss_map[i][j]=inv.RMSPE(current_resistance, etalon_resistance)
+
+    return loss_map
+
 
 if __name__  != '__main__':
     print('sensivity was imported')
